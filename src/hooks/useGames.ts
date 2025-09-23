fb5fd2a231e73b475f40b8e6a34d073d1d0a13ae
ChethanaMarasinghe <marasinghes850@gmail.com>
2025-09-23 06:36:44 +0530

    sort the games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -20,7 +20,10 @@ import { Genre } from "./useGenres";
 
 const useGames = (gameQuery: GameQuery) => 
         useData<Game>('/games',
-            {params: {genres: gameQuery.genre?.id, parent_platforms: gameQuery.platform?.id}}, 
+            {params: 
+                {genres: gameQuery.genre?.id, 
+                parent_platforms: gameQuery.platform?.id,
+                ordering: gameQuery.sortOrder}}, 
             [gameQuery]);
 export default useGames
 
